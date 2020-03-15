228e244cdf2a2dd5ff50a7e7be08532674a8ad5c
Exusia-i <519908095@qq.com>
2020-03-15 11:43:38 +0800

    update sendform.js

--- frontend/dist/js/sendform.js
+++ frontend/dist/js/sendform.js
@@ -1,12 +1,24 @@
 
 function isEmpty() {
-    var idNum = $("#idNum").val();
+
+    var iN = $("#idNum");
+    var tN = $("#telNum");
+
+    var idNum = iN.val();
+    var telNum = tN.val();
 
     if (idNum == '' || idNum == undefined || idNum == null) {
-        $("#idNum").css('background-color', 'rgba(255,0,0,0.1)');
+        iN.css('background-color', 'rgba(255,0,0,0.1)');
     }
     else {
-        $("#idNum").css('background-color', '')
+        iN.css('background-color', '')
+    }
+
+    if (telNum == '' || telNum == undefined || telNum == null) {
+        tN.css('background-color', 'rgba(255,0,0,0.1)');
+    }
+    else {
+        tN.css('background-color', '')
     }
 
 
