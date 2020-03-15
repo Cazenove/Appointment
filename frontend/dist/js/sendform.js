b78139e24194939d021e3accf101782244730fef
Exusia-i <519908095@qq.com>
2020-03-15 13:52:08 +0800

    验证输入的身份证号码与手机号码长度是否正确

--- frontend/dist/js/sendform.js
+++ frontend/dist/js/sendform.js
@@ -1,25 +1,56 @@
 
-function isEmpty() {
+function isEmpty(obj) {
+    var val = obj.value;
+    if (val == ''){
+        obj.setAttribute('style', 'background-color: rgba(255,0,0,0.1);')
+    }else {
+        obj.setAttribute('style', 'background-color: rgba(100,100,100,0.1);')
+    }
+}
 
+function warning(str) {
+    fyAlert.msg(str,{icon:2,animateType:2} )
+}
+
+function appointmentSubmit() {
     var iN = $("#idNum");
     var tN = $("#telNum");
 
     var idNum = iN.val();
     var telNum = tN.val();
 
-    if (idNum == '' || idNum == undefined || idNum == null) {
+    if ((idNum == '' || idNum == undefined || idNum == null)||(telNum == '' || telNum == undefined || telNum == null)) {
         iN.css('background-color', 'rgba(255,0,0,0.1)');
-    }
-    else {
-        iN.css('background-color', '')
-    }
-
-    if (telNum == '' || telNum == undefined || telNum == null) {
         tN.css('background-color', 'rgba(255,0,0,0.1)');
+        warning("未输入身份证号码或手机号码！");
+        return false;
+    }
+    else if (idNum.length != 18){
+        warning("身份证号码输入错误!");
+        return false;
+    }
+    else if (telNum.length != 11) {
+        warning("手机号码输入错误!");
+        return false;
     }
     else {
-        tN.css('background-color', '')
+        return true;
     }
-
-
 }
+
+function querySubmit() {
+    var sN = $("#stuNum");
+
+    var stuNum = sN.val();
+
+    if(stuNum == '' || stuNum == undefined || stuNum == null) {
+        sN.css('background-color', 'rgba(255,0,0,0.1)');
+        warning("未输入预约编号！");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
