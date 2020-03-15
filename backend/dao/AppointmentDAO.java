84f79c8ca3dd953c51b604468b0547f35e2f2f98
kuiqc <635816623@qq.com>
2020-03-15 17:47:10 +0800

    检查预约：如果手机号或者身份证号已经在本次摇号登记过了，预约失败

--- backend/dao/AppointmentDAO.java
+++ backend/dao/AppointmentDAO.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class AppointmentDAO {
 	public void doAppointmant() {
@@ -10,17 +11,33 @@ public class AppointmentDAO {
 		String phone_num = "111111";
 		int subscribe_num = 3;
 		
-		int rounds;
+		int rounds = 0;
 		
 		try {
 			Connection conn = DBUtil.getConnection();
-			String str = "select max(id) as id from appointment";
-			PreparedStatement pStmt = conn.prepareStatement(str);
+			
+			//获取当前轮次
+			String str_getRounds = "select max(id) as id from appointment";
+			PreparedStatement pStmt = conn.prepareStatement(str_getRounds);
 			ResultSet rSet = pStmt.executeQuery() ;
 			while(rSet.next()) {
 				//取得当前轮次
 				rounds = rSet.getInt("id");
 			}
+			
+			String str_getSameRound = "select id,appointment_id from appointment_list where id_number='"+id_num+" or phone_number='"+phone_num+"'";
+			pStmt = conn.prepareStatement(str_getSameRound);
+			rSet = pStmt.executeQuery() ;
+			while(rSet.next()) {
+				if(rounds==rSet.getInt("appointment_id")) {
+					//如果手机号或者身份证号已经在本次摇号登记过了，预约失败
+					System.out.println("手机号或者身份证号已经在本次摇号登记过了");
+					return;
+				} else if(rounds <= rSet.getInt("appointment_id")+3) {
+					//当用户有三轮内预约的记录时，更具记录的id查找draw_list
+				}
+			}
+			
 		} catch (SQLException e) {
 			System.out.print("ERROR!");
 			// TODO Auto-generated catch block
