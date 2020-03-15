85578e9d924818617f7794c7c7d65fa863240884
Etoilehey <895623136@qq.com>
2020-03-15 12:06:25 +0800

    -
    ---

--- frontend/dist/js/endappoint.js
+++ frontend/dist/js/endappoint.js
@@ -4,5 +4,16 @@ $(document).ready(function(){
 		$.post(url,{
 			status:"end"
 		},
+		function(status){//回调函数
+			if(status == 0) {
+				alert("成功结束预约！");
+			}
+			else if(status == 1) {
+				alert("预约已结束，请不要重复操作！");
+			}
+			else if(status == 2) {
+				alert("结束预约失败！");
+			}
+		});
 	});
 });
