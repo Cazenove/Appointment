5bb719c3bb6f209283c3a1ffa13bd4827629ea13
xwh <60315356+xwh130@users.noreply.github.com>
2020-03-15 13:00:44 +0800

    Update UserDao.java
    根据数据库表改动

--- backend/dao/UserDao.java
+++ backend/dao/UserDao.java
@@ -11,7 +11,7 @@ import util.DBUtil;
 
 public class UserDao {
     public int query(int numbers) {
-		String sql = "select * from user where **='"+numbers+"'";
+		String sql = "select * from draw_list where appointment_list_id='"+numbers+"'";
 		User user = new User();
         try (Connection c = DBUtil.getConnection(); Statement ps = c.prepareStatement(sql)) {
            
@@ -32,26 +32,24 @@ public class UserDao {
     }
     
     public User queryUser(int numbers) {
-		String sql = "select * from user where **='"+numbers+"'";
+		String sql = "select * from appointment_list where appointment_id='"+numbers+"'";
 		User user = new User();
         try (Connection c = DBUtil.getConnection(); Statement ps = c.prepareStatement(sql)) {
            
             ResultSet rs = ps.executeQuery(sql);
             while (rs.next()) {
             	user.setId(rs.getInt(1));
-            	user.setNum(rs.getInt(2));
-            	user.setTel(rs.getString(3));
-            	user.setName(rs.getString(4));
-            	user.setPurchase(rs.getInt(5));
+            	user.setName(rs.getString(2));
+            	user.setUserId(rs.getString(3));
+            	user.setTel(rs.getString(4));
+            	user.setPurchase(rs.getInt(5));	
+            	user.setNum(rs.getInt(6));
 			}
        
         } catch (SQLException e) {
 
             e.printStackTrace();
         }  
-
-        String id = ""+user.getId();
-        if (!id.equals(null)) return numbers;
-        else return 0;
+        return user;
     }
 }
